e29cc93b8f4ae8c7daba7f9f9d32f67ea1becc16
jinseongjoo <qpyou1234@naver.com>
2018-08-11 17:22:24 +0900

    Classes on Typescript part One

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const person = {
-    name: "evan",
-    age: 22,
-    gender: "male"
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const tim = new Human("Tim", 30, "female");
 const sayHi = (person) => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`);
 };
-console.log(sayHi(person));
+console.log(sayHi(tim));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-    name: string,
-    age: number,
-    gender: string
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name:string, age:number, gender?:string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "evan",
-    age: 22,
-    gender: "male"
-}
+const tim  = new Human("Tim", 30, "female")
 
 const sayHi = (person : Human):void => {
     console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`)
 }
 
-console.log(sayHi(person))
+console.log(sayHi(tim))
 
 export {};
