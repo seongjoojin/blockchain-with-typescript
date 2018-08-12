7aed4e64561085f0b755454641b4b1e62be02f1e
jinseongjoo <qpyou1234@naver.com>
2018-08-12 16:44:49 +0900

    Validating Block Structure

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,11 @@ class Block {
     }
 }
 Block.calculateBlockHash = (index, previousHash, timestamp, data) => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+Block.validateStructure = (aBlock) => typeof aBlock.index === "number" &&
+    typeof aBlock.hash === "string" &&
+    typeof aBlock.previousHash === "string" &&
+    typeof aBlock.timestamp === "number" &&
+    typeof aBlock.data === "string";
 Block.calculateBlockHash;
 const genesisBlock = new Block(0, "2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824", "D3CEC99112255DB9BF4963F7798562B6A36F1BD0B2016918629E007D474DEB70", "hello", 1533991463);
 let blockchain = [genesisBlock];
@@ -18,12 +23,22 @@ const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getNewTimeStamp = () => Math.round(new Date().getTime() / 1000);
 const crateNewBlock = (data) => {
-    const previosBlock = getLatestBlock();
-    const newIndex = previosBlock.index + 1;
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
     const newTimestamp = getNewTimeStamp();
-    const newHash = Block.calculateBlockHash(newIndex, previosBlock.hash, newTimestamp, data);
-    const newBlock = new Block(newIndex, newHash, previosBlock.hash, data, newTimestamp);
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
     return newBlock;
 };
-console.log(crateNewBlock("hello"), crateNewBlock("bye bye"));
+const isBlockValid = (candidateBlock, previousBlock) => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+};
 //# sourceMappingURL=index.js.map
